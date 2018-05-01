f41e980306e1b4c81428d731aaf528a0e043a6db
Mahshid M <mahshid@Mahshids-MacBook-Pro.local>
2018-05-01 16:34:16 -0700

    change

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+console.log('app is running')
+
+var template = React.createElement(
+  
+)
+
+<p>This is JSX from app.js!</p>;
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
